055098dd70dbd0646561949a3a8039d4cd4642e6
Silvia Kociánová <sissi.kocianova@gmail.com>
2024-06-28 10:38:15 +0200

    oops

---
+++ day-04-pomodoro-timer/src/Settings.js
@@ -0,0 +1,11 @@
+function Settings () {
+    return (
+        <div style={{textAlign:'left'}}>
+            <label>work minutes:</label>
+            <label>break minutes:</label>
+        </div>
+    );
+}
+
+
+export default Settings;
